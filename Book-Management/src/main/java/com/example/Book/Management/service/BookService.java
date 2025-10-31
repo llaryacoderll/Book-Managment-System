3920e72d38a36b42aa2abf519d7f1a2a8eb60d50
Arya choudhari <119004220+llaryacoderll@users.noreply.github.com>
2025-10-31 16:08:12 +0530

    Add files via upload

--- Book-Management/src/main/java/com/example/Book/Management/service/BookService.java
+++ Book-Management/src/main/java/com/example/Book/Management/service/BookService.java
@@ -3,12 +3,14 @@ package com.example.Book.Management.service;
 import com.example.Book.Management.exception.BookNotFoundException;
 import com.example.Book.Management.model.Book;
 import com.example.Book.Management.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookService {
+    @Autowired
     private BookRepository bookRepository;
 
     public Book addBook(Book book){
